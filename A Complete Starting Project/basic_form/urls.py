bbb74b5a16158b325a173690c7ae5f409430d814
dipu157 <dipu157@gmail.com>
2023-08-14 09:30:39 +0600

    working with form save

--- A Complete Starting Project/basic_form/urls.py
+++ A Complete Starting Project/basic_form/urls.py
@@ -3,5 +3,6 @@ from .views import *
 
 
 urlpatterns = [
-    path('', BasicFormIndex.as_view(), name='basic_form')
+    path('', BasicFormIndex.as_view(), name='basic_form'),
+    path("bform/new/", BasicFormCreateView.as_view(), name="bform_add"),
 ]
--- A Complete Starting Project/basic_form/views.py
+++ A Complete Starting Project/basic_form/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import View
+from .models import BasicForm
+from django.views.generic.edit import CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .forms import BaseFormCreateForm
@@ -10,3 +12,9 @@ class BasicFormIndex(LoginRequiredMixin,View):
     def get(self, request):
         form = BaseFormCreateForm()
         return render(request, "backend/basicForm/basic_form.html", {'form': form})
+
+
+class BasicFormCreateView(CreateView):
+    model = BasicForm
+    template_name = "backend/basicForm/basic_form.html"
+    fields = ["name", "code", "blood_group", "slug", "description", "image"]
