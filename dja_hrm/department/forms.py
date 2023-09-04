64c48e4ddbd8b2f0bd260461ad1208ce5e13f8b6
dipu157 <dipu157@gmail.com>
2023-09-04 10:47:25 +0600

    Department Crteate Done

--- dja_hrm/department/forms.py
+++ dja_hrm/department/forms.py
@@ -5,8 +5,9 @@ from .models import Department, Section
 class DepartmentCreateForm(forms.ModelForm):
     class Meta:
         model = Department
-        fields = ['name','short_name']
+        fields = ['department_code','name','short_name']
         widgets = {
+            'department_code': forms.TextInput(attrs={'class': 'form-control', 'id': 'codeid'}),
             'name': forms.TextInput(attrs={'class': 'form-control', 'id': 'nameid'}),
             'short_name': forms.TextInput(attrs={'class': 'form-control', 'id': 'snameid'}),
         }
