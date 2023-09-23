6d91fa29b667d2be1e6829a13149d7b3d3028393
dipu157 <dipu157@gmail.com>
2023-09-23 13:10:26 +0600

    roster admin aded

--- dja_hrm/hrm_project/settings.py
+++ dja_hrm/hrm_project/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'designation.apps.DesignationConfig',
     'employee.apps.EmployeeConfig',
     'shift.apps.ShiftConfig',
+    'roster.apps.RosterConfig',
 
     'crispy_forms', 
     'crispy_bootstrap5',
--- dja_hrm/roster/admin.py
+++ dja_hrm/roster/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from .models import DutyLocation, Roster
 
-# Register your models here.
+
+@admin.register(DutyLocation)
+class DutyLocationAdmin(admin.ModelAdmin):
+    list_display = ['location','description', 'status']
+    search_fields = ['status', 'location','description']
+    exclude = ['user'] 
+
+    def save_model(self, request, obj, form, change):
+        obj.user = request.user  # Assign the logged-in user
+        obj.save()
+    
+    class Meta:
+        db_table = 'duty_locations'
+        ordering = ['-created_at']
+        
+@admin.register(Roster)
+class RosterAdmin(admin.ModelAdmin):
+    list_display = ['employee','r_year', 'month_id', 'department']
+    search_fields = ['employee', 'r_year','month_id','department']
+    exclude = ['user'] 
+
+    def save_model(self, request, obj, form, change):
+        obj.user = request.user  # Assign the logged-in user
+        obj.save()
+    
+    class Meta:
+        db_table = 'rosters'
+        ordering = ['-created_at']
